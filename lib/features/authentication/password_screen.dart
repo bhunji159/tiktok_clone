8e645235d0fdd8e1ef565ac9f8a914eb755b01cc
bhunji159 <bhunji159@naver.com>
2023-02-27 09:54:39 +0900

    비밀번호 화면 완료

--- lib/features/authentication/password_screen.dart
+++ lib/features/authentication/password_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
@@ -34,13 +35,20 @@ class _PasswordScreemState extends State<PasswordScreen> {
     super.dispose();
   }
 
-  String? _isPasswordValid() {
-    if (_password.isEmpty) return null;
-    final regExp = RegExp(
-        r"(?=^.{8,20}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$");
+  bool _isPasswordLengthValid() {
+    if (_password.length < 8 || _password.length > 20) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-    if (!regExp.hasMatch(_password)) return "Not Vailid";
-    return null;
+  bool _isPasswordValid() {
+    final regExp = RegExp(
+        r"(?=^)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$");
+
+    if (!regExp.hasMatch(_password)) return false;
+    return true;
   }
 
   void _onScaffoldTap() {
@@ -48,7 +56,7 @@ class _PasswordScreemState extends State<PasswordScreen> {
   }
 
   void _onSubmit() {
-    if (_password.isEmpty || _isPasswordValid() != null) return;
+    if (!_isPasswordLengthValid() || !_isPasswordValid()) return;
     Navigator.of(context).push(MaterialPageRoute(
       builder: (context) => const PasswordScreen(),
     ));
@@ -102,7 +110,7 @@ class _PasswordScreemState extends State<PasswordScreen> {
                           size: Sizes.size20,
                         ),
                       ),
-                      Gaps.h16,
+                      Gaps.h10,
                       GestureDetector(
                         onTap: _toggleObscuredText,
                         child: FaIcon(
@@ -115,7 +123,6 @@ class _PasswordScreemState extends State<PasswordScreen> {
                       ),
                     ],
                   ),
-                  errorText: _isPasswordValid(),
                   hintText: "Password",
                   enabledBorder: UnderlineInputBorder(
                     borderSide: BorderSide(
@@ -130,12 +137,51 @@ class _PasswordScreemState extends State<PasswordScreen> {
                 ),
                 cursorColor: Theme.of(context).primaryColor,
               ),
+              Gaps.v10,
+              const Text(
+                'Your password must have:',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Gaps.v10,
+              Row(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.circleCheck,
+                    color: _isPasswordLengthValid()
+                        ? Colors.green
+                        : Colors.grey.shade400,
+                    size: Sizes.size20,
+                  ),
+                  Gaps.h5,
+                  const Text("8 to 20 characters"),
+                ],
+              ),
+              Gaps.v10,
+              Row(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.circleCheck,
+                    color: _isPasswordValid()
+                        ? Colors.green
+                        : Colors.grey.shade400,
+                    size: Sizes.size20,
+                  ),
+                  Gaps.h5,
+                  const AutoSizeText(
+                    'Letters, numbers, and special characters',
+                    maxLines: 1,
+                  ),
+                ],
+              ),
               Gaps.v16,
               GestureDetector(
-                  onTap: () => _onSubmit(),
-                  child: FormButton(
-                      disabled:
-                          _isPasswordValid() != null || _password.isEmpty)),
+                onTap: () => _onSubmit(),
+                child: FormButton(
+                  disabled: !_isPasswordLengthValid() || !_isPasswordValid(),
+                ),
+              ),
             ],
           ),
         ),
